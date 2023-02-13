04e1710894a3c42b737f8b221b51bcf901303896
suc_matsumoto-y <matsumoto-y@success-corp.co.jp>
2023-02-13 14:51:10 +0900

    [main] update

--- assets/js/mouse.js
+++ assets/js/mouse.js
@@ -11,6 +11,12 @@ class Mouse
 		document.addEventListener("mousedown", this._handleMousedown);
 		document.addEventListener("mouseup", this._handleMouseup);
 		document.addEventListener("mousemove", this._handleMousemove);
+		
+		document.addEventListener("touchstart", this._handleMousedown);
+		document.addEventListener("touchend", this._handleMouseup);
+		document.addEventListener("touchmove", this._handleMousemove);
+
+		//document.addEventListener('touchmove', this._noscroll, {passive: false});
 	}
 
 	dispose = () =>
@@ -18,6 +24,12 @@ class Mouse
 		document.removeEventListener("mousedown", this._handleMousedown);
 		document.removeEventListener("mouseup", this._handleMouseup);
 		document.removeEventListener("mousemove", this._handleMousemove);
+
+		document.removeEventListener("touchstart", this._handleMousedown);
+		document.removeEventListener("touchend", this._handleMouseup);
+		document.removeEventListener("touchmove", this._handleMousemove);
+
+		//document.removeEventListener('touchmove', this._noscroll);
 	}
 
 	update()
@@ -26,20 +38,29 @@ class Mouse
 		this._old_y = this._y;
 	}
 
+	_noscroll(e)
+	{
+		e.preventDefault();
+	}
+
 	_handleMousedown = (e) =>
 	{
 		this._is_down = true;
+		console.log("down");
 	}
 	_handleMouseup = (e) =>
 	{
 		this._is_down = false;
+		console.log("up");
 	}
 	_handleMousemove = (e) =>
 	{
+		e.preventDefault();
+
 		this._old_x = this._x;
 		this._old_y = this._y;
-		this._x = e.pageX;
-		this._y = e.pageY;
+		this._x = e.pageX == undefined ? e.changedTouches[0].pageX : e.pageX;;
+		this._y = e.pageY == undefined ? e.changedTouches[0].pageY : e.pageY;
 	}
 
 	getPosX() { return this._x; }
